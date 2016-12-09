6138cd1ff0d3b01ad621bd33f50e35a6b2cc628a
Marcos Marado <mindboosternoori@gmail.com>
2016-12-09 15:01:00 +0000

    quem-morreu.sh agora recebe ano como argumento

--- quem-morreu.sh
+++ quem-morreu.sh
@@ -1,7 +1,24 @@
+#!/bin/bash
+
 # script that lists all authors that died in a certain year
 # (looks at both lists)
 
-# TODO - get this as an argument
-YEAR=1945
+# Validate if we have the correct number of arguments
+if [ $# -ne 1 ]; then
+	echo "Usage: $0 <year>"
+	exit 1
+fi
 
-echo "$(cat dglb/autores.csv |cut -d\; -f2-5|grep $YEAR$ ; grep $YEAR bnp/autores-quase-garantidamente-falecidos.csv|grep -v $YEAR-)" |sort -u
+# Check if <year> is a valid argument
+if ! [[ "$1" =~ ^[0-9][0-9][0-9][0-9]$ ]]; then
+	echo "The <year> argument must be a four digits number (eg: 1950)."
+	exit 2
+fi
+
+echo "$(
+	cat dglb/autores.csv | \
+	cut -d\; -f2-5| \
+	grep $1$ ; grep $1 bnp/autores-quase-garantidamente-falecidos.csv| \
+	grep -v $1- | \
+	grep -v "$1 -"
+)" |sort -u
