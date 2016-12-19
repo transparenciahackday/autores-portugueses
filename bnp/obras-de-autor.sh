14f5eb27ab3ee7959525d303ee65d2f34499a112
Marcos Marado <mindboosternoori@gmail.com>
2016-12-19 21:35:06 +0000

    bnp/obras-de-autor.sh: fix output
    removed debug string from output

--- bnp/obras-de-autor.sh
+++ bnp/obras-de-autor.sh
@@ -8,7 +8,7 @@ echo "<b>$autor</b>"
 for r in $(
   grep "rdaa:P50111 \"$autor\"" a0511.ttl -B3|grep theeuropeanlibrary|cut -d\< -f2|cut -d\> -f1
 ); do
-  obra=$(echo "$r :"; grep "$r> a <" a0511.ttl -A6|grep rdam:P30156|cut -d\" -f2);
+  obra=$(grep "$r> a <" a0511.ttl -A6|grep rdam:P30156|cut -d\" -f2);
   echo "<li>$obra</li>";
 done
 echo "</p>"
