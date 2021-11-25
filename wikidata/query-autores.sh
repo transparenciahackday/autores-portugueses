67ed0ed086aa2959d5e36c5cfdd88a93f2a27d7b
Marcos Marado <marcos@bymason.com>
2021-11-25 12:56:38 +0000

    wikidata: split query to make it lighter
    The script doesn't time out anymore: we stopped querying the label
    service, and instead just grab each of the author's items... and then
    query for the names, one by one.

--- wikidata/query-autores.sh
+++ wikidata/query-autores.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-# The query we want to make
-
 year=0;
 if [ "$1" != "" ]; then
 	year=$(($1-71));
@@ -14,21 +11,37 @@ fi
 
 # Portuguese authors whose works enter the public domain in $year
 query='
-SELECT DISTINCT ?item ?itemLabel WHERE {
+SELECT DISTINCT ?item WHERE {
   ?item wdt:P31 wd:Q5.
   ?item wdt:P27 wd:Q45.
   ?item wdt:P570 ?time0. hint:Prior hint:rangeSafe true.
   FILTER((?time0 >= "'$year'-01-01"^^xsd:dateTime) && (?time0 < "'$((year+1))'-01-01"^^xsd:dateTime))
   ?item (wdt:P106/wdt:P279*) wd:Q482980.
-  SERVICE wikibase:label { bd:serviceParam wikibase:language "pt". }
 }
 '
 
-echo "$query" | ./query-wikidata.sh > autores.xml || echo "Error trying to query wikidata!" && exit 0
+echo "$query" | ./query-wikidata.sh > autores.xml || (echo "Error trying to query wikidata!" && exit 0)
 echo "autores.xml gerado"
 
+# We didn't query with the label service to be lighter on our queries... so now
+# we'll need to ask for the names of each author item:
+echo -n "" > autores-e-nomes.xml
+while read -r line
+do
+  query='
+    SELECT ?label
+    WHERE {
+    wd:'$line' rdfs:label ?label .
+    FILTER (langMatches(lang(?label), "pt"))
+    }
+  ';
+  echo "$query" | ./query-wikidata.sh >> autores-e-nomes.xml || (echo "Error trying to query wikidata!" && exit 0);
+done < <(grep entity autores.xml |cut -d/ -f5|cut -d \< -f1)
+echo "autores-e-nomes.xml gerado"
+
+# Let's turn this into the expected csv
 while read -r line
 do
   echo "\"$line\";\"\";\"$year\"";
-done < <(grep literal autores.xml |cut -d\> -f2|cut -d \< -f1) > autores.csv
+done < <(grep lang=\'pt\' autores-e-nomes.xml |cut -d\> -f2|cut -d \< -f1) > autores.csv
 echo "autores.csv gerado"
