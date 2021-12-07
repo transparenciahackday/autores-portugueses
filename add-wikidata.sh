bb0e396e1fa287f98dd9bc4a24bf0d5009c59d7f
Marcos Marado <mindboosternoori@gmail.com>
2021-12-07 23:16:56 +0000

    add-wikidata.sh dá preferência à wikidata
    Os resultados do wikidata têm o campo "wikidata" preenchido, os outros
    não, pelo que há que dar preferência ao que vem do wikidata.

--- add-wikidata.sh
+++ add-wikidata.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 ## For now, wikidata source isn't consolidated in any way. This script hacks
-## around it. It assumes a morreu-em-1948.csv exists locally (with the dead
+## around it. It assumes a morreu-em-1951.csv exists locally (with the dead
 ## authors from other sources) and an wikidata/autores.csv also exists (due a
 ## previous run of wikidata scripts).
 
+# TODO: read filename insead of using an hardcoded one
+
+cp morreu-em-1951.csv sem-wikidata.csv
+
 while read -r author
 do
-	if [ "$(grep -i "$author" morreu-em-1948.csv |wc -l)" -eq "0" ]; then
-		grep "$author" wikidata/autores.csv;
+	if [ "$(grep -i "$author" sem-wikidata.csv |wc -l)" -ne "0" ]; then
+		grep -i -v "$author" sem-wikidata.csv > tmp; mv tmp sem-wikidata.csv
 	fi;
+	grep "$author" wikidata/autores.csv;
 done < <(cat wikidata/autores.csv|cut -d\" -f2) > com-wikidata.csv
-cat morreu-em-1948.csv >> com-wikidata.csv
+cat sem-wikidata.csv >> com-wikidata.csv
 
 echo "com-wikidata.csv generated"
