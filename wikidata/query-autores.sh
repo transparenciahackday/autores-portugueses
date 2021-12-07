095394fe01f293662d8b75a7f61837a85299c866
Marcos Marado <mindboosternoori@gmail.com>
2021-12-07 19:50:58 +0000

    wikidata: also save wikidata's id for the author
    We are also breaking the first query in two (more precisely, we are
    not filtering out authors, and once we get dead people and query them
    one by one, we also check out of they were authors), to be less heavy
    on wikidata queries' service (we do more queries, but lighter ones).
    
    Now the resulting wikidata/autores.csv file will be in the following
    format:
    
    "author";"year of birth";"year of death";"wikidata item"
    
    (This is the previous format, with an added column, so it does not
    break the rest of the scripts)

--- wikidata/query-autores.sh
+++ wikidata/query-autores.sh
@@ -9,22 +9,22 @@ else
 	exit;
 fi
 
-# Portuguese authors whose works enter the public domain in $year
+# Portuguese people whose works enter the public domain in $year
 query='
 SELECT DISTINCT ?item WHERE {
   ?item wdt:P31 wd:Q5.
   ?item wdt:P27 wd:Q45.
   ?item wdt:P570 ?time0. hint:Prior hint:rangeSafe true.
   FILTER((?time0 >= "'$year'-01-01"^^xsd:dateTime) && (?time0 < "'$((year+1))'-01-01"^^xsd:dateTime))
-  ?item (wdt:P106/wdt:P279*) wd:Q482980.
 }
 '
 
-echo "$query" | ./query-wikidata.sh > autores.xml || (echo "Error trying to query wikidata!" && exit 0)
-echo "autores.xml gerado"
+echo "$query" | ./query-wikidata.sh > autores.xml || (echo "Error trying to query wikidata!" && exit 1);
+test $? -ne 0 && exit 1;
 
 # We didn't query with the label service to be lighter on our queries... so now
-# we'll need to ask for the names of each author item:
+# we'll need to ask for the names of each author item.
+# Also, we didn't really filter them out by authors! Doing it now, too...
 echo -n "" > autores-e-nomes.xml
 while read -r line
 do
@@ -32,16 +32,24 @@ do
     SELECT ?label
     WHERE {
     wd:'$line' rdfs:label ?label .
+    wd:'$line' (wdt:P106/wdt:P279*) wd:Q482980.
     FILTER (langMatches(lang(?label), "pt"))
-    }
+    } LIMIT 1
   ';
-  echo "$query" | ./query-wikidata.sh >> autores-e-nomes.xml || (echo "Error trying to query wikidata!" && exit 0);
+  echo "$line" >> autores-e-nomes.xml;
+  echo "$query" | ./query-wikidata.sh >> autores-e-nomes.xml || (echo "Error trying to query wikidata!" && exit 1);
+  test $? -ne 0 && exit 1;
 done < <(grep entity autores.xml |cut -d/ -f5|cut -d \< -f1)
 echo "autores-e-nomes.xml gerado"
 
+# Let's turn this into a csv (wikidata;name)
+grep literal -B9 autores-e-nomes.xml|sed 's/\t//g'|tr --delete '\n'|sed 's/--/\n/g'| \
+  sed "s/<?xml version='1.0' encoding='UTF-8'?><sparql xmlns='http:\/\/www.w3.org\/2005\/sparql-results#'><head><variable name='label'\/><\/head><results><result><binding name='label'><literal xml:lang='pt'>/;/g"| \
+  sed "s/<\/literal>//g" > autores-temp.csv
+echo "" >> autores-temp.csv
+
+
 # Let's turn this into the expected csv
-while read -r line
-do
-  echo "\"$line\";\"\";\"$year\"";
-done < <(grep lang=\'pt\' autores-e-nomes.xml |cut -d\> -f2|cut -d \< -f1) > autores.csv
+# "Author Name";"Year of Birth";"Year of Death";"Wikidata Item"
+while read -r line; do wd=$(echo $line|cut -d\; -f1); name=$(echo $line|cut -d\; -f2); echo "\"$name\";\"\";\"$year\";\"$wd\""; done < autores-temp.csv > autores.csv
 echo "autores.csv gerado"
