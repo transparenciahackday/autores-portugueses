187983e286d0ab023bc1b1e0f2f6e11216ebc92b
Marcos Marado <mindboosternoori@gmail.com>
2021-12-07 22:41:36 +0000

    wikidata: also fetch year of birth
    Closes: #18

--- wikidata/query-autores.sh
+++ wikidata/query-autores.sh
@@ -11,10 +11,11 @@ fi
 
 # Portuguese people whose works enter the public domain in $year
 query='
-SELECT DISTINCT ?item WHERE {
+SELECT DISTINCT ?item (year(?birthdate) as ?birthyear) WHERE {
   ?item wdt:P31 wd:Q5.
   ?item wdt:P27 wd:Q45.
   ?item wdt:P570 ?time0. hint:Prior hint:rangeSafe true.
+  OPTIONAL{?item wdt:P569 ?birthdate .}
   FILTER((?time0 >= "'$year'-01-01"^^xsd:dateTime) && (?time0 < "'$((year+1))'-01-01"^^xsd:dateTime))
 }
 '
@@ -28,28 +29,41 @@ test $? -ne 0 && exit 1;
 echo -n "" > autores-e-nomes.xml
 while read -r line
 do
+  item=$(echo "$line"|cut -d\; -f1);
+  yob=$(echo "$line"|cut -d\; -f2);
   query='
     SELECT ?label
     WHERE {
-    wd:'$line' rdfs:label ?label .
-    wd:'$line' (wdt:P106/wdt:P279*) wd:Q2500638.
+    wd:'$item' rdfs:label ?label .
+    wd:'$item' (wdt:P106/wdt:P279*) wd:Q2500638.
     FILTER (langMatches(lang(?label), "pt"))
     } LIMIT 1
   ';
   echo "$line" >> autores-e-nomes.xml;
   echo "$query" | ./query-wikidata.sh >> autores-e-nomes.xml || (echo "Error trying to query wikidata!" && exit 1);
   test $? -ne 0 && exit 1;
-done < <(grep entity autores.xml |cut -d/ -f5|cut -d \< -f1)
+done < <(grep entity autores.xml -A3|sed 's/\t//g'|tr --delete '\n'|sed 's/--/\n/g'|sed "s/<uri>http:\/\/www.wikidata.org\/entity\///g"|sed "s/<\/uri><\/binding><binding name='birthyear'><literal datatype='http:\/\/www.w3.org\/2001\/XMLSchema#integer'>/;/g"|cut -d\< -f1)
 echo "autores-e-nomes.xml gerado"
 
-# Let's turn this into a csv (wikidata;name)
+# Let's turn this into a csv (wikidata;yob;name)
 grep literal -B9 autores-e-nomes.xml|sed 's/\t//g'|tr --delete '\n'|sed 's/--/\n/g'| \
   sed "s/<?xml version='1.0' encoding='UTF-8'?><sparql xmlns='http:\/\/www.w3.org\/2005\/sparql-results#'><head><variable name='label'\/><\/head><results><result><binding name='label'><literal xml:lang='pt'>/;/g"| \
   sed "s/<\/literal>//g" > autores-temp.csv
 echo "" >> autores-temp.csv
 
+# Actually, if the item didn't have an date of birth, we have an entry in the
+# format "wikidata;name" where it should be "wikidata;;name"... Let's fix that:
+while read -r line; do
+  fields="${line//[^;]}";
+  if [ "${#fields}" -eq 2 ]; then
+    echo "$line";
+  else
+    echo "$(echo "$line"|cut -d\; -f1);;$(echo "$line"|cut -d\; -f2)";
+  fi
+done < autores-temp.csv > autores-temp-fixed.csv
+mv autores-temp-fixed.csv autores-temp.csv
 
 # Let's turn this into the expected csv
 # "Author Name";"Year of Birth";"Year of Death";"Wikidata Item"
-while read -r line; do wd=$(echo $line|cut -d\; -f1); name=$(echo $line|cut -d\; -f2); echo "\"$name\";\"\";\"$year\";\"$wd\""; done < autores-temp.csv > autores.csv
+while read -r line; do wd=$(echo $line|cut -d\; -f1); yob=$(echo $line|cut -d\; -f2); name=$(echo $line|cut -d\; -f3); echo "\"$name\";\"$yob\";\"$year\";\"$wd\""; done < autores-temp.csv > autores.csv
 echo "autores.csv gerado"
